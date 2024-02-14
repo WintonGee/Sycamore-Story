6807e189ef4327944ed32ec401d3f60ba87052fa
Winton Gee <wgee01@calpoly.edu>
2024-02-14 13:38:32 -0800

    Refractor Code

--- Sycamore Story/Ned.java
+++ Sycamore Story/Ned.java
@@ -1,14 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Ned here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Ned extends PhysicsActor
-{
-    private static final int SCROLL_WIDTH = 160, SCROLL_HEIGHT = 160;
+public class Ned extends PhysicsActor {
+    private static final int SCROLL_WIDTH = 160;
     private static final float JUMP = 8.0f, WALK = 4.0f;
    
     private int absoluteScroll, initialXPosition, initialYPosition;
@@ -17,8 +10,7 @@ public class Ned extends PhysicsActor
     private GreenfootSound walkSound, powSound;
     private boolean resetting;
     
-    public Ned()
-    {
+    public Ned() {
         super("images/warrior", ".png", 7, 2);
         absoluteScroll = 0;
         punching = false;
@@ -31,167 +23,158 @@ public class Ned extends PhysicsActor
     }
     
     @Override
-    protected void addedToWorld(World world)
-    {
+    protected void addedToWorld(World world) {
         initialXPosition = getX();
         initialYPosition = getY();
     }
     
     @Override
-    protected void useDefaultOrientation()
-    {
-        if(!isDefaultOrientation())
-        {
-            ninjaPunch.mirrorHorizontally();
-        }
-        
+    protected void useDefaultOrientation() {
+        if (!isDefaultOrientation()) ninjaPunch.mirrorHorizontally();
         super.useDefaultOrientation();
     }
     
     @Override
-    protected void useFlippedOrientation()
-    {
-        if(isDefaultOrientation())
-        {
-            ninjaPunch.mirrorHorizontally();
-        }
-        
+    protected void useFlippedOrientation() {
+        if (isDefaultOrientation()) ninjaPunch.mirrorHorizontally();
         super.useFlippedOrientation();
     }
     
-    public boolean isPunching()
-    {
+    public boolean isPunching() {
         return punching;
     }
     
-    /**
-     * Act - do whatever the Ned wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        if(Greenfoot.isKeyDown("space") && onGround)
-        {
+    public void act() {
+        handleJump();
+        handlePunch();
+        // handleMovementKeys();
+        handleAnimation();
+        adjustCamera();
+        super.act();
+        handleReset();
+    } 
+    
+    public void reset() {
+        resetting = true;
+    }
+    
+    public void kill() {
+        walkSound.stop();
+    }
+    
+    public void adjustCamera() {
+        NinjaWorld world = (NinjaWorld) getWorld();
+        resetCameraPosition();
+        
+        if (Greenfoot.isKeyDown("r")) reset();
+        
+        if (resetting) {
+            setCameraOnReset(world);
+        }
+        
+        handleScrolling(world);
+    }
+    
+    private void handleJump() {
+        if (Greenfoot.isKeyDown("space") && onGround) {
             velocity.y -= JUMP;
             onGround = false;
             getWorld().addObject(new SpeechBubble("jump-bubble.png", this, .5), -200, -200);
         }
-        
-        if(Greenfoot.isKeyDown("control"))
-        {
-            if(!punching)
-            {
-                punching = true;
-                useSpecialImage(ninjaPunch);
-                getWorld().addObject(new SpeechBubble("pow-bubble.png", this, 0.3), -200, -200);
-                powSound.play();
-            }
-        }
-        else
-        {
-            if(Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left"))
-            {
-                if(!isAnimating())
-                {
-                    animate();
-                }
-                useDefaultOrientation();
-                velocity.x = -WALK;
-            }
-            else if(Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right"))
-            {
-                if(!isAnimating())
-                {
-                    animate();
-                }
-                useFlippedOrientation();
-                velocity.x = WALK;
-            }
-            else
-            {
-                if(isAnimating() || punching)
-                {
-                    rest();
-                }
-                velocity.x = 0.0f;
-            }
-            
+    }
+    
+    private void handlePunch() {
+        if (Greenfoot.isKeyDown("control")) {
+            handlePunchKeyDown();
+        } else {
+            handleMovementKeys();
             punching = false;
         }
-        
-        if(!isAnimating() && punching && onGround)
-        {
-            velocity.x = 0.0f;
+    }
+    
+    private void handleMovementKeys() {
+        if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) handleLeftMovement();
+        else if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) handleRightMovement();
+        else handleNoMovement();
+    }
+    
+    private void handlePunchKeyDown() {
+        if (!punching) {
+            punching = true;
+            useSpecialImage(ninjaPunch);
+            getWorld().addObject(new SpeechBubble("pow-bubble.png", this, 0.3), -200, -200);
+            powSound.play();
         }
-        
-        if(isAnimating())
-        {
-            if(!walkSound.isPlaying())
-            {
-                walkSound.playLoop();
-            }
-        }
+    }
+    
+    private void handleLeftMovement() {
+        handleMovementAction(-WALK, true);
+    }
+    
+    private void handleRightMovement() {
+        handleMovementAction(WALK, false);
+    }
+    
+    private void handleNoMovement() {
+        if (isAnimating() || punching) rest();
+        velocity.x = 0.0f;
+    }
+    
+    private void handleMovementAction(float walkDirection, boolean defaultOrientation) {
+        if (!isAnimating()) animate();
+        // setOrientation(walkDirection, defaultOrientation);
+        if (defaultOrientation) 
+            useDefaultOrientation();
         else
-        {
-            if(walkSound.isPlaying())
-            {
-                walkSound.stop();
-            }
-        }
+            useFlippedOrientation();
+        velocity.x = walkDirection;
+    }
+    
+    private void handleAnimation() {
+        if (isAnimating()) playWalkSound();
+        else stopWalkSound();
         
-        adjustCamera();
-        
-        super.act();
-        
-        if(resetting)
-        {
+        if (!isAnimating() && punching && onGround) velocity.x = 0.0f;
+    }
+    
+    private void playWalkSound() {
+        if (!walkSound.isPlaying()) walkSound.playLoop();
+    }
+    
+    private void stopWalkSound() {
+        if (walkSound.isPlaying()) walkSound.stop();
+    }
+    
+    private void handleReset() {
+        if (resetting) {
             setLocation(initialXPosition, initialYPosition);
             // Call save here
         }
         
         resetting = false;
-    } 
-    
-    public void reset()
-    {
-        resetting = true;
     }
     
-    public void kill()
-    {
-        walkSound.stop();
-    }
-    
-    public void adjustCamera() {
-        NinjaWorld world = ((NinjaWorld)getWorld());
+    private void resetCameraPosition() {
+        NinjaWorld world = (NinjaWorld) getWorld();
         world.setCameraX(0);
         world.setCameraY(0);
-        
-        if(Greenfoot.isKeyDown("r"))
-        {
-            reset();
-        }
-        
-        if(resetting)
-        {
-            world.setCameraX(-absoluteScroll);
-            absoluteScroll = 0;
-            velocity.x = 0.0f;
-            velocity.y = 0.0f;
-            useDefaultOrientation();
-        }
-        
-        // Scrolling
-        if((getX() < SCROLL_WIDTH) && (world.getWorldX() < world.getWorldWidth() / 2 - 5))
-        {
+    }
+    
+    private void setCameraOnReset(NinjaWorld world) {
+        world.setCameraX(-absoluteScroll);
+        absoluteScroll = 0;
+        velocity.x = 0.0f;
+        velocity.y = 0.0f;
+        useDefaultOrientation();
+    }
+    
+    private void handleScrolling(NinjaWorld world) {
+        if (getX() < SCROLL_WIDTH && world.getWorldX() < world.getWorldWidth() / 2 - 5) {
             world.setCameraX(SCROLL_WIDTH - getX());
             absoluteScroll += SCROLL_WIDTH - getX();
-        }
-        else if((getX() > world.getWidth() - SCROLL_WIDTH) && (world.getWorldX() > -690))
-        {
+        } else if (getX() > world.getWidth() - SCROLL_WIDTH && world.getWorldX() > -690) {
             world.setCameraX(world.getWidth() - SCROLL_WIDTH - getX());
             absoluteScroll += world.getWidth() - SCROLL_WIDTH - getX();
         }
     }
-    
 }
