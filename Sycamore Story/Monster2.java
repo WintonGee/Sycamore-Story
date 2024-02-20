08045a84c6dd7b3b96b228740b787bdc76cdc3b5
Joshua Levin <thejoryle@gmail.com>
2024-02-19 22:30:42 -0800

    added new monster classes, needs images

---
+++ Sycamore Story/Monster2.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class OscillatingPlatform here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Monster2 extends Monster
+{
+    public Monster2(int xRange, int spawnX, int spawnY)
+    {
+        super(xRange, 0, 2, "images/warrior", ".png", 7, 2, spawnX, spawnY);
+        for(GreenfootImage gfi : this.images)
+        {
+            gfi.scale(50, 50);
+        }
+    }
+    
+    /**
+     * Act - do whatever the OscillatingPlatform wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        super.act();
+    }    
+}
